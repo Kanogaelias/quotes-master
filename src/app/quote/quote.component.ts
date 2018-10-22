75edb0cd2f9f371458e72de592f5cb2f76b0915f
Kanogaelias <kanogae@gmail.com>
2018-10-22 13:05:48 +0300

    add output

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,10 +12,10 @@ export class QuoteComponent implements OnInit {
     new Quote(3,"Panua paja mkwaju waja","leso ya bibi","binti shoka",0,0,new Date(2013,6,22)),
     new Quote(4," If you cannot do great things, do small things in a great way.","Napoleon Hill","Tugi",0,0,new Date(2014,9,20)),
     new Quote(5,"Let us be grateful to people who make us happy, they are the charming gardeners who make our souls blossom.","Marcel","dre",0,0,new Date(2015,9,21)),
-    new Quote(6,"The supreme art of war is to subdue the enemy without fighting.","Sun","Tzu",0,0,new Date(2018,9,21)),
-    new Quote(7,"The supreme art of war is to subdue the enemy without fighting.","Sun","Tzu",0,0,new Date(2018,9,21)),
-    new Quote(8,"The supreme art of war is to subdue the enemy without fighting.","Sun","Tzu",0,0,new Date(2018,9,21)),
-    new Quote(9,"The supreme art of war is to subdue the enemy without fighting.","Sun","Tzu",0,0,new Date(2018,9,21)),
+    new Quote(6,"The supreme art of war is to subdue the enemy without fighting.","Sun","Tzu",0,0,new Date(2016,9,21)),
+    new Quote(7,"Always remember that you are absolutely unique. Just like everyone else.","Clinton","Okerio",0,0,new Date(2018,9,21)),
+    new Quote(8,"You are closer to death now than then"," Cynthia","Muriithi",0,0,new Date(2018,9,21)),
+    new Quote(9,"Shave,like face,like head.","Principal","FJ",0,0,new Date(2018,9,21)),
     new Quote(10,"The supreme art of war is to subdue the enemy without fighting.","Sun","Tzu",0,0,new Date(2018,9,21)),
         ];
   toggleDetails(index){
